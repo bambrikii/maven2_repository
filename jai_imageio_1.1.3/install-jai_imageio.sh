7fe3a7e818dbf94b27633c9201e4e4a93a2aab46
Ալեքսանդր Առաքելյան <bambrikii@gmail.com>
2015-07-16 20:30:22 +0300

    - artifactIds with underscores.

--- jai_imageio_1.1.3/install-jai_imageio.sh
+++ jai_imageio_1.1.3/install-jai_imageio.sh
@@ -7,6 +7,6 @@ REPOSITORY_URL=scp://maven@192.168.11.192:/home/maven/.m2/repository
 #REPOSITORY_ID=local-repository
 #REPOSITORY_URL=scp://asd@127.0.0.1:/home/asd/temp/.m2/repository
 
-mvn deploy:deploy-file -DgroupId=$GROUP_ID -DartifactId=jai-codec -Dversion=1.1.3 -Dpackaging=$PACKAGING -Dfile=jai_codec-1.1.3.jar -DrepositoryId=$REPOSITORY_ID -Durl=$REPOSITORY_URL
-mvn deploy:deploy-file -DgroupId=$GROUP_ID -DartifactId=jai-core -Dversion=1.1.3 -Dpackaging=$PACKAGING -Dfile=jai_core-1.1.3.jar -DrepositoryId=$REPOSITORY_ID -Durl=$REPOSITORY_URL
-mvn deploy:deploy-file -DgroupId=$GROUP_ID -DartifactId=jai-imageio -Dversion=1.1 -Dpackaging=$PACKAGING -Dfile=jai_imageio-1.1.jar -DrepositoryId=$REPOSITORY_ID -Durl=$REPOSITORY_URL
+mvn deploy:deploy-file -DgroupId=$GROUP_ID -DartifactId=jai_codec -Dversion=1.1.3 -Dpackaging=$PACKAGING -Dfile=jai_codec-1.1.3.jar -DrepositoryId=$REPOSITORY_ID -Durl=$REPOSITORY_URL
+mvn deploy:deploy-file -DgroupId=$GROUP_ID -DartifactId=jai_core -Dversion=1.1.3 -Dpackaging=$PACKAGING -Dfile=jai_core-1.1.3.jar -DrepositoryId=$REPOSITORY_ID -Durl=$REPOSITORY_URL
+mvn deploy:deploy-file -DgroupId=$GROUP_ID -DartifactId=jai_imageio -Dversion=1.1 -Dpackaging=$PACKAGING -Dfile=jai_imageio-1.1.jar -DrepositoryId=$REPOSITORY_ID -Durl=$REPOSITORY_URL
